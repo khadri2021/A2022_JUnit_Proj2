8bfa229b4b857380b42b79d591525bb35d3805b2
khadri2021 <khadri.institutions@gmail.com>
2024-01-18 08:46:08 +0530

    updated code

--- src/test/java/com/khadri/spring/controller/DataControllerTest.java
+++ src/test/java/com/khadri/spring/controller/DataControllerTest.java
@@ -1,13 +1,15 @@
 package com.khadri.spring.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,8 @@ import com.khadri.spring.service.DataService;
 
 @ExtendWith(MockitoExtension.class)
 class DataControllerTest {
+
+	private static final String WISH_MSG = "Welcome to Junit";
 
 	@Mock
 	private DataService dataService;
@@ -30,11 +34,10 @@ class DataControllerTest {
 
 	@Test
 	void testDataAccepts() {
-
-		Mockito.when(dataService.wish()).thenReturn("Welcome to Junit");
-
+		when(dataService.wish()).thenReturn(WISH_MSG);
 		ResponseEntity<String> actualResponse = dataController.dataAccepts();
-		assertEquals("Welcome to Junit", actualResponse.getBody());
+		assertNotNull(actualResponse);
+		assertEquals(WISH_MSG, actualResponse.getBody());
+		assertNotEquals(WISH_MSG + "HAI", actualResponse.getBody());
 	}
-
 }
